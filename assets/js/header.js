5ea1f2c01e3b20fc0b75cc08f8907c51de722f5c
sebivh <sebivhelm@gmail.com>
2023-12-11 23:51:34 +0100

    Added Submenus for the Burger Menu

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,14 +1,24 @@
-
 registerOnLoad(setUpBurger);
+registerOnLoad(setUpSubMenus);
 
 function setUpBurger() {
-    //Code to be executeted when the document html is loaded
+    //Code to be executed when the document html is loaded
     let nav_element = document.querySelector("nav");
     let burger = nav_element.querySelector(".burger");
     let link_list = nav_element.querySelector(".menu-navbar-container");
 
-    burger.onclick = function () {
+    burger.onclick = function() {
         link_list.classList.toggle("active");
         burger.classList.toggle("active");
     }
+}
+
+function setUpSubMenus() {
+    let sub_menus = document.querySelectorAll("nav .menu-navbar-container #menu-navbar li.menu-item-has-children");
+
+    sub_menus.forEach(m => {
+        m.querySelector("a").onclick = function() {
+            m.classList.toggle("active");
+        }
+    });
 }
